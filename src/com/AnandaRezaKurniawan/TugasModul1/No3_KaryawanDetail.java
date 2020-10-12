f0f8f9eac25de6541314a02b3583a251c42a2309
ananda19104061 <72422050+ananda19104061@users.noreply.github.com>
2020-10-12 13:38:45 +0700

    Initial commit

---
+++ src/com/AnandaRezaKurniawan/TugasModul1/No3_KaryawanDetail.java
@@ -0,0 +1,28 @@
+package com.AnandaRezaKurniawan.TugasModul1;
+
+public class No3_KaryawanDetail {
+    private static String nama = "Ananda Reza Kurniawan";
+    private static String ttl = "Pekalongan, 30 Agustus 2000";
+    private static String agama = "Islam";
+    private static String alamat = "Jln. Mondstadt Liyue";
+    private static String kelamin = "Pria Bukan kaleng kaleng";
+    private static String status = "Bentar lagi Menikah";
+    private static String riwayat= "S1 Software Engineering";
+    private static String email = "rezaananda@gmail.com";
+    private static String nomer = "08156156111";
+
+    public static void main(String[] args) {
+
+        // Hasil output Biodata Karyawan PT Genshin Impact
+        System.out.println("===========Biodata Karyawan PT Genshin Impact===========");
+        System.out.println("Nama Lengkap\t\t : " + nama);
+        System.out.println("TTL\t\t\t\t\t : " + ttl);
+        System.out.println("Alamat\t\t\t\t : " + alamat);
+        System.out.println("Jenis Kelamin\t\t : " + kelamin);
+        System.out.println("Status Pernikahan\t : " + status);
+        System.out.println("Pendidikan Terakhir\t : " + riwayat);
+        System.out.println("Email\t\t\t\t : " + email);
+        System.out.println("Nomer HP\t\t\t : " + nomer);
+    }
+}
+
